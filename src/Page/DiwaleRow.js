456cf745fd72be3195c4236abf094b1f1ae0834b
unknown <abdulkaderjilani120@gmail.com>
2022-07-27 03:21:41 +0600

    added sound

--- src/Page/DiwaleRow.js
+++ src/Page/DiwaleRow.js
@@ -1,11 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import {Howl, Howler , seek} from 'howler';
 const DiwaleRow = ({song , index}) => {
-    const {audioSong , songTitle , singer , actorMale , actorFemale , movieName} = song
+    const [id , setId] = useState("")
+    const [isPlaying, setIsPlaying] = useState(false);
+    const {audioSong , songTitle , singer , actorMale , actorFemale , movieName , image, _id} = song
+    
+    const soundHandle =(src ) =>{             
+        const sound = new Howl({
+            src,
+            html5: true         
+           
+          });
+         
+      sound.play(id);
+        
+       
+         
+    }
+    const handlePause = (src) => {
+        const sound = new Howl({
+            src,
+            html5: true ,      
+            mute: true
+          });         
+        sound.pause()
+       
+      }
+    Howler.volume(0.5);
     return (
-        <tr>
-        <th>{index + 1}</th>
-        <td><audio controls src={audioSong}></audio></td>
+        <tr className='row-head' >
+        <th>{index + 1}</th>        
+        <td onClick={() =>soundHandle(audioSong , setId(_id)) } ><img className='song-image' src={image} alt="song Image" /></td>
+         <td onClick={() =>handlePause(audioSong) } >Stop sound</td>
         <td>{songTitle}</td>
         <td>{singer}</td>
         <td>{actorMale}</td>
